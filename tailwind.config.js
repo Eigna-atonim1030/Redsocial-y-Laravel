68b0ebcbeecffdd1704fd4126dd45e05e62c2387
Eigna <79377944+Eigna-atonim1030@users.noreply.github.com>
2023-12-02 10:05:31 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [require("@tailwindcss/forms"), require('flowbite/plugin')],
+};
