8b19cf8425bed828bfbfcb7accf653b8ce2b887f
Eigna <79377944+Eigna-atonim1030@users.noreply.github.com>
2023-12-02 09:39:19 -0500

    Add files via upload

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
@@ -15,13 +14,11 @@
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    <!-- Dark Mode Button -->
+    <!-- Dark Mode Script -->
     <script>
-        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia(
+        if (localStorage.getItem('color-theme') === 'dark' || (!localStorage.getItem('color-theme') && window.matchMedia(
                 '(prefers-color-scheme: dark)').matches)) {
             document.documentElement.classList.add('dark');
-        } else {
-            document.documentElement.classList.remove('dark')
         }
     </script>
 </head>
@@ -34,30 +31,9 @@
             </a>
         </div>
 
-        <div
-            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
             {{ $slot }}
         </div>
-
-        <!-- MarcosKlender Footer -->
-        <footer
-            class="fixed bottom-0 left-0 z-20 w-full h-1 p-4 bg-white border-t border-gray-200 shadow flex items-center justify-center md:p-6 dark:bg-gray-800 dark:border-gray-600">
-            <p class="inline-flex items-center text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                <span class="mr-1">
-                    Hecho con
-                </span>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                    class="w-6 h-6 text-red-500">
-                    <path fill-rule="evenodd"
-                        d="M12.963 2.286a.75.75 0 00-1.071-.136 9.742 9.742 0 00-3.539 6.177A7.547 7.547 0 016.648 6.61a.75.75 0 00-1.152-.082A9 9 0 1015.68 4.534a7.46 7.46 0 01-2.717-2.248zM15.75 14.25a3.75 3.75 0 11-7.313-1.172c.628.465 1.35.81 2.133 1a5.99 5.99 0 011.925-3.545 3.75 3.75 0 013.255 3.717z"
-                        clip-rule="evenodd" />
-                </svg>
-                <span class="ml-1">
-                    por <a href="https://marcosklender.com/" class="hover:underline text-blue-600 dark:text-blue-500" target="_blank">MarcosKlender</a>
-                </span>
-            </p>
-        </footer>
-        
     </div>
 </body>
 
